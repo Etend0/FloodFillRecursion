fe730f1738e8f10403ad171fddbd13dcb1743116
Etendo <etendo123@gmail.com>
2026-07-12 21:12:18 -0600

    Add project files.

---
+++ FloodFillRecursion/Models/CellModel.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Elijah Hodge
+ * CST - 250
+ * 07/12/2026
+ * Flood Fill Recursion
+ * Activity 3
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace FloodFillRecursion.Models
+{
+    internal class CellModel
+    {
+        // Cell Model Properties
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public string Contents { get; set; }
+
+        /// <summary>
+        /// Parameterized constructor for CellModel
+        /// </summary>
+        /// <param name="row"></param>
+        /// <param name="column"></param>
+        /// <param name="contents"></param>
+        public CellModel(int row, int column, string contents)
+        {
+            Row = row;
+            Column = column;
+            Contents = contents;
+        }
+    }
+}
---
+++ FloodFillRecursion/Program.cs
@@ -0,0 +1,8 @@
+﻿/*
+ * Elijah Hodge
+ * CST - 250
+ * 07/12/2026
+ * Flood Fill Recursion
+ * Activity 3
+*/
+
